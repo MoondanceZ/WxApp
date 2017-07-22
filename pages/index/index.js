e637fa7fa37faa4c25d0fd83c5ba5e3edf209709
MoondanceZ <QYK_ZZ@hotmail.com>
2017-07-22 23:09:28 +0800

    update index

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,37 @@
 var app = getApp()
 Page({
   data: {
+     ConsumeRecord: [{
+      Date: "7月19日",
+      Type: 1,
+      Amount: 1001,
+      Info: "吃饭"
+    }, {
+      Date: "7月19日",
+      Type: 1,
+      Amount: 1001,
+      Info: "吃饭"
+    }, {
+      Date: "7月11日",
+      Type: 1,
+      Amount: 1001,
+      Info: "吃饭"
+    }, {
+      Date: "7月12日",
+      Type: 1,
+      Amount: 1001,
+      Info: "吃饭"
+    }, {
+      Date: "7月13日",
+      Type: 1,
+      Amount: 1001,
+      Info: "吃饭"
+    },{
+      Date: "7月13日",
+      Type: 2,
+      Amount: 5001,
+      Info: "工资"
+    }],
     userInfo: {}
   },
   //事件处理函数
