f9dc00af93bc6ce1b8cfc1dc69f95e6a3480f8ed
MoondanceZ <QYK_zZ@hotmail.com>
2018-02-08 16:37:13 +0800

    Update

--- app.js
+++ app.js
@@ -1,30 +1,64 @@
+//app.js
+const openIdUrl = require('./config').loginUrl
 App({
-  onLaunch: function() {
-    //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-  },
+  onLaunch: function () {
+    // 展示本地存储能力
+    // var logs = wx.getStorageSync('logs') || []
+    // logs.unshift(Date.now())
+    // wx.setStorageSync('logs', logs)
 
-  getUserInfo: function(cb) {
-    var that = this
-    if (this.globalData.userInfo) {
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    } else {
-      //调用登录接口
-      wx.getUserInfo({
-        withCredentials: false,
-        success: function(res) {
-          that.globalData.userInfo = res.userInfo
-          typeof cb == "function" && cb(that.globalData.userInfo)
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        var _self = this;
+        if (!res.authSetting['scope.userInfo']) {
+          wx.authorize({
+            scope: 'scope.userInfo',
+            success() {
+              // 登录
+              wx.login({
+                success: res => {
+                  console.log(res);
+                  // 发送 res.code 到后台换取 openId, sessionKey, unionId
+                  wx.request({
+                    url: openIdUrl,
+                    data: {
+                      code: res.code
+                    },
+                    header: {
+                      'content-type': 'application/json' // 默认值
+                    },
+                    method: "GET",
+                    success: res => {
+                      var result = res.data;
+                      if (result.IsSuccess) {
+                        _self.globalData.userId = result.Data.Id;
+                        _self.globalData.token = result.Data.Token;
+                      } else {
+                        wx.showToast({
+                          title: result.Message,
+                          icon: 'none',
+                          duration: 2000
+                        })
+                      }
+                    }
+                  })
+                }
+              })
+            },
+            fail() {
+              wx.redirectTo({
+                url: '../logs/logs'
+              })
+            }
+          })
         }
-      })
-    }
+      }
+    })
   },
-
   globalData: {
-    hasLogin: true,
     userInfo: null,
-    openId: null
-  }  
+    userId: null,
+    token: null
+  }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,40 +1,13 @@
 //index.js
 //获取应用实例
-var app = getApp()
+const app = getApp()
+
 Page({
   data: {
-     ConsumeRecord: [{
-      Date: "7月19日",
-      Type: 1,
-      Amount: 1001,
-      Info: "吃饭"
-    }, {
-      Date: "7月19日",
-      Type: 1,
-      Amount: 1001,
-      Info: "吃饭"
-    }, {
-      Date: "7月11日",
-      Type: 1,
-      Amount: 1001,
-      Info: "吃饭"
-    }, {
-      Date: "7月12日",
-      Type: 1,
-      Amount: 1001,
-      Info: "吃饭"
-    }, {
-      Date: "7月13日",
-      Type: 1,
-      Amount: 1001,
-      Info: "吃饭"
-    },{
-      Date: "7月13日",
-      Type: 2,
-      Amount: 5001,
-      Info: "工资"
-    }],
-    userInfo: {}
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
   bindViewTap: function() {
@@ -43,14 +16,39 @@ Page({
     })
   },
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
       })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
     })
   }
 })
